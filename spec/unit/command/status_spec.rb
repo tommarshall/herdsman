cc90740daa905afaaa9c3ba0a0d1dcf76c28dec4
Tom Marshall <tommarshall7@gmail.com>
2017-02-22 20:18:06 +0000

    Split status command unit test phases

--- spec/unit/command/status_spec.rb
+++ spec/unit/command/status_spec.rb
@@ -4,6 +4,7 @@ require 'herdsman/command/status'
 describe Herdsman::Command::Status do
   it 'implements the command interface' do
     command = described_class.new
+
     expect(command).to respond_to(:run)
   end
 end
